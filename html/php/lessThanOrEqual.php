c1a76cdd5074296d4693de14848ee75891401583
Zachary Stevens <zed2808@gmail.com>
2016-05-15 18:24:15 -0400

    Searching for shop max stock checks for > 0 results

--- html/php/lessThanOrEqual.php
+++ html/php/lessThanOrEqual.php
@@ -18,27 +18,32 @@ $stock = $_REQUEST['stock'];
 $query = "SELECT * FROM shop WHERE stock <= '$stock'";
 
 // Execute query
-$result = mysqli_query($conn, $query);
+if($result = mysqli_query($conn, $query)) {
+	if(mysqli_num_rows($result) > 0) {
+		// Build result string
+		$display_string = "<table border=\"1\" cellpadding=\"3\">";
+		$display_string .= "<tr>";
+		$display_string .= "<th>Item</th>";
+		$display_string .= "<th>Price</th>";
+		$display_string .= "<th>Stock</th>";
+		$display_string .= "</tr>";
 
-// Build result string
-$display_string = "<table border=\"1\" cellpadding=\"3\">";
-$display_string .= "<tr>";
-$display_string .= "<th>Item</th>";
-$display_string .= "<th>Price</th>";
-$display_string .= "<th>Stock</th>";
-$display_string .= "</tr>";
+		// Insert a new row in the table for each product returned
+		while($row = mysqli_fetch_array($result)) {
+			$display_string .= "<tr>";
+			$display_string .= "<td>$row[item]</td>";
+			$display_string .= "<td>$row[price]</td>";
+			$display_string .= "<td>$row[stock]</td>";
+			$display_string .= "</tr>";
+		}
 
-// Insert a new row in the table for each product returned
-while($row = mysqli_fetch_array($result)) {
-	$display_string .= "<tr>";
-	$display_string .= "<td>$row[item]</td>";
-	$display_string .= "<td>$row[price]</td>";
-	$display_string .= "<td>$row[stock]</td>";
-	$display_string .= "</tr>";
+		$display_string .= "</table>";
+	} else {
+		$display_string = "<p>Query returned 0 results.</p>";
+	}
+} else {
+	$display_string = "<p>ERROR: Could not query database.</p>" . mysqli_error($conn);
 }
-
-//echo "Query: " . $query . "<br />";
-$display_string .= "</table>";
 
 echo $display_string;
 ?>
